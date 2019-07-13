e45daf0d99fbe7468543a6725c42da1fa9871bb2
Di Ye <imdiye@gmail.com>
2019-07-12 19:34:37 -0700

    inicio dos

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
-const http = require('http')
+const bodyParser = require('body-parser')
+const express = require('express')
 
-const hostname = '127.0.0.1'
-const port = 3000
+try {
+	require('dotenv').config()
+} catch (error) {
+	console.warn('unable to load .env')
+}
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World\n')
+const PORT = process.env.PORT || 3030
+
+const app = express()
+app.use(bodyParser.json())
+app.use(express.static('build'))
+
+app.get('/', (req, res) => {
+	res.send('yo world')
 })
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+app.use('*', function(req, res) {
+	res.status(404).json({ message: 'Not Found' })
 })
+
+let server
+
+function runServer(port = PORT) {
+	return new Promise((resolve, reject) => {
+		server = app.listen(port, () => {
+			console.log(`Your app is listening on port ${port}`)
+			resolve()
+		})
+	})
+}
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err))
+}
+
+module.exports = { app, runServer }
