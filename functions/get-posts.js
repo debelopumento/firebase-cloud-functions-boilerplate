f36340e6be4c5f873e56e7ab16f75f9c5217a1d0
Di Ye <imdiye@gmail.com>
2019-10-06 18:41:54 -0700

    Code cleanup

--- functions/get-posts.js
+++ functions/get-posts.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const co = require('co')
 const mongodb = require('mongodb')
 
 try {
@@ -13,16 +12,12 @@ const DATABASE_URL = process.env.DATABASE_URL
 const functions = require('firebase-functions')
 
 exports.getLatestPosts = functions.https.onRequest(async (req, res) => {
-	co(async () => {
-		const client = await mongodb.MongoClient.connect(DATABASE_URL)
+	const client = await mongodb.MongoClient.connect(DATABASE_URL)
 
-		const docs = await client
-			.db('dcf')
-			.collection('posts')
-			.find()
-			.toArray()
-		res.send(JSON.stringify(docs))
-	}).catch(error => {
-		res.send('Error: ' + error.toString())
-	})
+	const docs = await client
+		.db('dcf')
+		.collection('posts')
+		.find()
+		.toArray()
+	res.send(JSON.stringify(docs))
 })
